8fd77b3c446b5dad3ea81bf4a5801ccb47547d72
Miquel Colomer <mcolomer@gmail.com>
2025-05-30 20:17:09 +0200

    Improve start

--- start_n8n.sh
+++ start_n8n.sh
@@ -15,5 +15,16 @@ if [ ! -d "$HOME/.n8n/custom/node_modules/n8n-nodes-document-generator" ]; then
     npm link n8n-nodes-document-generator
 fi
 
+if ! command -v npm &> /dev/null; then
+    echo "npm command not found. Please install with nvm following https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating"
+    exit 1
+fi
+
+#exists n8n binary?
+if ! command -v n8n &> /dev/null; then
+    echo "n8n command not found. Please install n8n globally using 'npm install -g n8n'."
+    exit 1
+fi
+
 #n8n start --tunnel
 n8n start
